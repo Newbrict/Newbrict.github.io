c19353822b67fdb7f160881202c40a72dcb7974d
Dimitar Dimitrov <newbrict@gmail.com>
2014-04-18 12:58:57 -0400

    updated website

---
+++ js/utils.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	// jQuery SmoothScroll | Version 18-04-2013
+	$('a[href*=#]').click(function() {
+		if($(this).parent().hasClass('carousel') || $(this).attr('href').indexOf('javascript')>-1) return;
+		// duration in ms
+		var duration=1000;
+		// easing values: swing | linear
+		var easing='swing';
+		// get / set parameters
+		var newHash=this.hash;
+		var oldLocation=window.location.href.replace(window.location.hash, '');
+		var newLocation=this;
+		// make sure it's the same location
+		if(oldLocation+newHash==newLocation) {
+			// get target
+			var target=$(this.hash+', a[name='+this.hash.slice(1)+']').offset().top;
+			// adjust target for anchors near the bottom of the page
+			if(target > $(document).height()-$(window).height()) target=$(document).height()-$(window).height();
+			// animate to target and set the hash to the window.location after the animation
+			$('html, body').animate({ scrollTop: target }, duration, easing, function() {
+				// add new hash to the browser location
+				window.location.href=newLocation;
+			});
+			// cancel default click action
+			return false;
+		}
+	});
+});
+
+$(document).ready(function() {
+	for (var j = 0; j < 18; j++) {
+		var yeehah = function() {
+			var i = j;
+			setTimeout(function() {
+				var n = "new";
+				var o = "brict" + "@g";
+				var b = "mail.com"
+				var o = n + o + b;
+				var t = document.getElementById('email');
+				var s = t.innerHTML;
+				t.innerHTML = s.slice(0,i) + o[i] + s.slice(i+1,s.length);
+			}, Math.floor(Math.random()*1500));
+		}();
+	}
+});
